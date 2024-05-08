de7573cdd0b87de51219107f7ed0edbfbeea22d1
vikas-851 <167636013+vikas-851@users.noreply.github.com>
2024-05-08 06:20:40 +0530

    Update userbot.py

--- VIPMUSIC/core/userbot.py
+++ VIPMUSIC/core/userbot.py
@@ -65,10 +65,10 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("THE_VIP_BOY")
-                await self.one.join_chat("THE_VIP_BOY_OP")
-                await self.one.join_chat("TG_FRIENDSS")
-                await self.one.join_chat("VIP_CREATORS")
+                await self.one.join_chat("DEADLY_NETWORK")
+                await self.one.join_chat("DEADLY_GIVEWAY")
+                await self.one.join_chat("DEADLY_NETWORK")
+                await self.one.join_chat("Deadly_Community")
 
             except:
                 pass
@@ -95,10 +95,10 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("THE_VIP_BOY")
-                await self.two.join_chat("THE_VIP_BOY_OP")
-                await self.two.join_chat("TG_FRIENDSS")
-                await self.two.join_chat("VIP_CREATORS")
+                await self.two.join_chat("DEADLY_NETWORK")
+                await self.two.join_chat("DEADLY_GIVEWAY")
+                await self.two.join_chat("DEADLY_NETWORK")
+                await self.two.join_chat("Deadly_Community")
             except:
                 pass
             assistants.append(2)
@@ -119,10 +119,10 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("THE_VIP_BOY")
-                await self.three.join_chat("THE_VIP_BOY_OP")
-                await self.three.join_chat("TG_FRIENDSS")
-                await self.three.join_chat("VIP_CREATORS")
+                await self.three.join_chat("DEADLY_NETWORK")
+                await self.three.join_chat("DEADLY_GIVEWAY")
+                await self.three.join_chat("DEADLY_NETWORK")
+                await self.three.join_chat("Deadly_Community")
             except:
                 pass
             assistants.append(3)
@@ -142,10 +142,10 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("THE_VIP_BOY")
-                await self.four.join_chat("THE_VIP_BOY_OP")
-                await self.four.join_chat("TG_FRIENDSS")
-                await self.four.join_chat("VIP_CREATORS")
+                await self.four.join_chat("DEADLY_NETWORK")
+                await self.four.join_chat("DEADLY_GIVEWAY")
+                await self.four.join_chat("DEADLY_NETWORK")
+                await self.four.join_chat("Deadly_Community")
             except:
                 pass
             assistants.append(4)
@@ -165,10 +165,10 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("THE_VIP_BOY")
-                await self.five.join_chat("THE_VIP_BOY_OP")
-                await self.five.join_chat("TG_FRIENDSS")
-                await self.five.join_chat("dhhdshhss6")
+                await self.five.join_chat("DEADLY_NETWORK")
+                await self.five.join_chat("DEADLY_GIVEWAY")
+                await self.five.join_chat("DEADLY_NETWORK")
+                await self.five.join_chat("Deadly_Community")
             except:
                 pass
             assistants.append(5)
